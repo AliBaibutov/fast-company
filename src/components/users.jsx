e3317578967f776d10a237c6952834bfa6bfe15e
AliKhan <baybutov95@mail.ru>
2022-07-19 20:23:18 +0500

    task-1

---
+++ src/components/users.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import api from "../api";
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+
+  const handleDelete = (userId) => {};
+
+  const renderPhrase = (number) => {};
+};
+export default Users;
