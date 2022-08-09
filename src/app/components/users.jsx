7dba42831782a9ead467c8a9c34392846f11703c
AliKhan <baybutov95@mail.ru>
2022-08-10 00:06:05 +0500

    написан метод сортировки

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -2,16 +2,18 @@ import React, { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import SearchStatus from "./searchStatus";
-import User from "./user";
+import UserTable from "./userTable";
 import api from "../api";
 import PropTypes from "prop-types";
 import GroupList from "./groupList";
+import _ from "lodash";
 
 const Users = ({ users, onDelete, onBookmark }) => {
-    const pageSize = 4;
+    const pageSize = 8;
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfession] = useState();
     const [selectedProf, setSelectedProf] = useState();
+    const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" });
     useEffect(() => {
         api.professions.fetchAll().then((data) => setProfession(data));
     }, []);
@@ -21,6 +23,17 @@ const Users = ({ users, onDelete, onBookmark }) => {
 
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
+    };
+
+    const handleSort = (item) => {
+        if (sortBy.iter === item) {
+            setSortBy((prevState) => ({
+                ...prevState,
+                order: prevState.order === "asc" ? "desc" : "asc"
+            }));
+        } else {
+            setSortBy({ iter: item, order: "asc" });
+        }
     };
 
     const handleProfessionSelect = (item) => {
@@ -36,13 +49,14 @@ const Users = ({ users, onDelete, onBookmark }) => {
         : users;
 
     const count = filteredUsers.length;
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order]);
     const startIndex = (currentPage - 1) * pageSize;
     let newCurrentPage = currentPage;
-    if (startIndex === filteredUsers.length) {
+    if (startIndex === sortedUsers.length) {
         newCurrentPage = newCurrentPage - 1;
         setCurrentPage(newCurrentPage);
     }
-    const userCrop = paginate(filteredUsers, newCurrentPage, pageSize);
+    const userCrop = paginate(sortedUsers, newCurrentPage, pageSize);
     const clearFilter = () => {
         setSelectedProf();
     };
@@ -66,29 +80,12 @@ const Users = ({ users, onDelete, onBookmark }) => {
             <div className="d-flex flex-column">
                 <SearchStatus length={count} />
                 {count > 0 && (
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Имя</th>
-                                <th scope="col">Качества</th>
-                                <th scope="col">Профессия</th>
-                                <th scope="col">Встретился, раз</th>
-                                <th scope="col">Оценка</th>
-                                <th scope="col">Избранное</th>
-                                <th />
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {userCrop.map((user) => (
-                                <User
-                                    key={user._id}
-                                    {...user}
-                                    onDelete={onDelete}
-                                    onBookmark={onBookmark}
-                                />
-                            ))}
-                        </tbody>
-                    </table>
+                    <UserTable
+                        users={userCrop}
+                        onSort={handleSort}
+                        onDelete={onDelete}
+                        onBookmark={onBookmark}
+                    />
                 )}
                 <div className="d-flex justify-content-center">
                     <Pagination
